a435e37b55ad3872cdb71e5c7568209fc5f4c67a
Quentin CHAMPEAU <quentin_ch@hotmail.fr>
2026-04-28 01:55:36 +0200

    Run formating in backend

--- backend/src/WebApi/Domain/Entities/Expense.cs
+++ backend/src/WebApi/Domain/Entities/Expense.cs
@@ -5,8 +5,8 @@ public sealed class Expense
     public Guid Id { get; set; }
     public string Description { get; set; } = string.Empty;
     public decimal Amount { get; set; }
-    public string Category  { get; set; } = string.Empty;
-    public DateTime Date  { get; set; }
-    public DateTime CreatedAtUtc  { get; set; }
-    public DateTime UpdatedAtUtc  { get; set; }
+    public string Category { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public DateTime CreatedAtUtc { get; set; }
+    public DateTime UpdatedAtUtc { get; set; }
 }
